ffe5d7cae91d19063487fea15e1b3a0d88a4dcbf
HendriPermana <hendripermana60@gmail.com>
2022-12-05 23:02:04 +0700

    update seed plane

--- seeders/20221203104944-demo-plane.js
+++ seeders/20221203104944-demo-plane.js
@@ -8,189 +8,78 @@ module.exports = {
       [
         {
           namePlane: "ABX Air",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           namePlane: "Aer Lingus",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           namePlane: "Air Adriatic",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           namePlane: "Air Anatolia",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           namePlane: "Air Atlanta Icelandic",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           namePlane: "Air Finland",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           namePlane: "Air France",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           namePlane: "AIr Inuit",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           namePlane: "Air Japan",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           namePlane: "Air Srpska",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           namePlane: "Air Tindi",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           namePlane: "Air Transat",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           namePlane: "Alliance Airlines",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
-          namePlane: "ATA",
+          namePlane: "Surabaya Airlines",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
-          namePlane: "AOM French Airlines",
-        },
-        {
-          namePlane: "Arrow Air",
-        },
-        {
-          namePlane: "Aviogenex",
-        },
-        {
-          namePlane: "Belavia",
-        },
-        {
-          namePlane: "BEXAIR",
-        },
-        {
-          namePlane: "Brigenair",
-        },
-        {
-          namePlane: "British Airways",
-        },
-        {
-          namePlane: "British Caledonian",
-        },
-        {
-          namePlane: "Britannia Airways",
-        },
-        {
-          namePlane: "Capitol Air",
-        },
-        {
-          namePlane: "Carnival Air Lines",
-        },
-        {
-          namePlane: "Cargolux",
-        },
-        {
-          namePlane: "Champion Air",
-        },
-        {
-          namePlane: "Citybird",
-        },
-        {
-          namePlane: "EuroAtlantic Airways",
-        },
-        {
-          namePlane: "Finnair",
-        },
-        {
-          namePlane: "First Choice Airways",
-        },
-        {
-          namePlane: "Frontier Flying Service",
-        },
-        {
-          namePlane: "Futura",
-        },
-        {
-          namePlane: "Garuda Indonesia",
-        },
-        {
-          namePlane: "Germania",
-        },
-        {
-          namePlane: "Hapag-Lloyd",
-        },
-        {
-          namePlane: "Helios Airways",
-        },
-        {
-          namePlane: "Japan Airlines",
-        },
-        {
-          namePlane: "JMC",
-        },
-        {
-          namePlane: "Kelowna Flightcraft",
-        },
-        {
-          namePlane: "Kenn Borek Air",
-        },
-        {
-          namePlane: "Ladeco",
-        },
-        {
-          namePlane: "Lion Air",
-        },
-        {
-          namePlane: "Lot Polish Airlines",
-        },
-        {
-          namePlane: "Luzair",
-        },
-        {
-          namePlane: "Martinair",
-        },
-        {
-          namePlane: "Miami Air",
-        },
-        {
-          namePlane: "Monarch Airlines",
-        },
-        {
-          namePlane: "MyTravel Airways",
-        },
-        {
-          namePlane: "Omni Air International",
-        },
-        {
-          namePlane: "Ozjet",
-        },
-        {
-          namePlane: "North American Airlines",
-        },
-        {
-          namePlane: "Palmair",
-        },
-        {
-          namePlane: "Skyservice",
-        },
-        {
-          namePlane: "Starair",
-        },
-        {
-          namePlane: "Sterling European Airlines",
-        },
-        {
-          namePlane: "Sobelair",
-        },
-        {
-          namePlane: "Tame",
-        },
-        {
-          namePlane: "Tango&nbsp;(divisi&nbsp;Air Canada)",
-        },
-        {
-          namePlane: "TransMeridian Airlines",
-        },
-        {
-          namePlane: "UPS",
-        },
-        {
-          namePlane: "White",
+          namePlane: "Yogjakarta Airlines",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
       ],
       {}
