c802a900cfbf6f9447f9cce89f09bedaf321d999
DewaBiara <angabiara@gmail.com>
2022-12-16 17:54:41 +0800

    Change Google Cloud Key

--- uploadImage/HandlerFile.js
+++ uploadImage/HandlerFile.js
@@ -3,7 +3,7 @@ const { format } = require("util");
 const { Storage } = require("@google-cloud/storage");
 
 const storage = new Storage({ keyFilename: "google-cloud-key.json" });
-const bucket = storage.bucket("platinum-project-backend");
+const bucket = storage.bucket("platinum-project-backend.appspot.com");
 
 const uploadPictures = async (req, res) => {
   try {
