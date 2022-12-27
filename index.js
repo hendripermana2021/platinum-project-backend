c5a55e6dafe25082b6d7d754f10229ff53e3755c
HendriPermana <hendripermana60@gmail.com>
2022-12-27 21:32:53 +0700

    Adding Unit Testing

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ import router from "./routes/index.js";
 dotenv.config();
 const app = express();
 import bodyParser from "body-parser";
-import { createServer } from "http";
-// import { Server } from "socket.io";
 
 app.use(cors());
 app.use(cookieParser());
@@ -16,55 +14,10 @@ app.use(router);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-import { Server } from "socket.io";
-
-//socket IO
-const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  transports: ["polling"],
-  cors: {
-    origin: "*",
-  },
-});
-
-let onlineUsers = [];
-
-const addNewUser = (username, socketId) => {
-  !onlineUsers.some((user) => user.username === username) &&
-    onlineUsers.push({ username, socketId });
-};
-
-const removeUser = (socketId) => {
-  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
-};
-
-const getUser = (username) => {
-  return onlineUsers.find((user) => user.username === username);
-};
-
-io.on("connection", (socket) => {
-  socket.on("newUser", (username) => {
-    //FUNCTION FOR SAVE DATA ONLINE USERS
-    addNewUser(username, socket.id);
-    console.log("user connected");
-  });
-
-  socket.on("sendNotification", ({ senderName, receiverName }) => {
-    const receiver = getUser(receiverName);
-    io.to(receiver.socketId).emit("getNotification", {
-      senderName,
-    });
-  });
-
-  socket.on("disconnect", () => {
-    removeUser(socket.id);
-  });
-});
-
-global.io = io;
+export const application = app;
 
 const { PORT = 8000 } = process.env;
 
-httpServer.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("Listening on port", PORT);
 });
